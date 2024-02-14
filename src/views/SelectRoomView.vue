60ddf06f2ac9ba8c121185f975a2fecbe18f0dd5
Kittiyaporn Ngamlert <64160232@go.buu.ac.th>
2024-02-14 16:20:35 +0700

    create SelectRoomPage

---
+++ src/views/SelectRoomView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card-container grid grid-cols-2 md:grid-cols-3 gap-4">
+
+    <button class="btn-guest">
+        <span class="inline-block align-top text-left">Guest</span>
+    </button>
+    <button class="btn-date">
+        <span class="inline-block align-top text-left">Check in</span>
+    </button>
+    <!-- <div inline-datepicker data-date="02/25/2022"></div> -->
+    <button class="btn-date">
+        <span class="inline-block align-top text-left">Check out</span>
+    </button>
+
+</div>
+</template>
+<style scoped>
+.card-container {
+    width: 60%;
+    max-width: auto;
+}
+
+.btn-guest,
+.btn-date {
+    border-color: #000000;
+    background-color: #FFFFFF;
+    padding: 10px 20px;
+    border-radius: 8px;
+    box-shadow: 0px 4px 6px rgb(0 0  0/0.25);
+    font-weight: medium;
+    display: inline-block;
+    width: 229px;
+    margin: 10px;
+}
+</style>
