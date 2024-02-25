82ced99d6876516898e8a28d6fd244e5c3a1a2e4
Kantinan  Srikasem <64160040@go.buu.ac.th>
2024-02-25 17:27:39 +0700

    Update user authentication logic

--- src/model/activity.model.ts
+++ src/model/activity.model.ts
@@ -4,12 +4,13 @@ export interface Activity {
   name: string;
   price: number;
   description: string;
+  image:string;
 }
 
 export interface ActivityPerBook {
   id: number;
   qty: number;
-  image:string;
+ 
   total: number;
   activities: Activity;   
 }
