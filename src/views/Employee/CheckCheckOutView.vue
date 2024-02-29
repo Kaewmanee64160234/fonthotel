175df0b4abbdb657100eb29aba6ba82f7144307a
Titapon Keawbua <64160047@go.buu.ac.th>
2024-02-29 14:31:04 +0700

    checkcheckout not complete btn fine

--- src/views/Employee/CheckCheckOutView.vue
+++ src/views/Employee/CheckCheckOutView.vue
@@ -12,7 +12,12 @@ const clickback = () => {
             <button @click="clickback">
                 <i style="font-size: 30px" class="far">&#xf359;</i>
             </button>
+            <button class="btn-checkout justify-end">
+                <a>Check out</a>
+            </button>
+
         </div>
+    
         <div class="min-h-screen flex card-container">
             <!-- Left Side: -->
             <div class="flex-1 flex flex-col pt-3 p-10">
@@ -73,36 +78,39 @@ const clickback = () => {
                                 <p>-</p>
                             </div>
                         </div>
+                        <div class="flex-7 flex flex-row pt-2 px-5" style="font-size: 13px;">
+                            <div class="flex-1 flex flex-col">
+                                <p class="font-medium">Fine</p>
+                                <p>-</p>
+                            </div>
+                            <div class="flex-2 flex flex-col">
+                                <p>-</p>
+                            </div>
+                        </div>
+                        <div class="pt-3">
+                            <hr class="color-line " />
+                        </div>
+
 
                         <div class="flex-9 flex flex-row pt-3 px-5 " style="font-size: 20px;">
                             <div class="flex-1 flex flex-col">
-                                <p class="font-medium">Total:</p>
+                                <p class="font-medium">Discount:</p>
                             </div>
                             <div class="flex-2 flex flex-col">
-                                <p class="font-medium">THB 7,009.00</p>
+                                <p class="font-medium"> THB 1,500.00</p>
                             </div>
-                        </div>
-
-                        <div class="pt-3">
-                            <hr class="color-line" />
                         </div>
 
                         <div class="flex-9 flex flex-row pt-2 px-5 " style="font-size: 20px;">
                             <div class="flex-1 flex flex-col">
-                                <p class="font-medium">Cash pledge</p>
+                                <p class="font-medium  mt-5">Total: </p>
                             </div>
                             <div class="flex-2 flex flex-col">
-                                <p class="font-medium">THB 1,500.00</p>
+                                <p class="font-medium mt-5">THB 5,509.00</p>
                             </div>
                         </div>
 
-                        <div class="flex-8 flex flex-row pt-2 px-5" style="font-size: 12px;">
-                            <div class="flex-1 flex flex-col">
-                                <p class="text-red-500">(This cash pledge is confirmation of your stay at our hotel.
-                                    You will receive a refund when you check out. As for the room rate, you will
-                                    have to pay it when you check out.)</p>
-                            </div>
-                        </div>
+
 
                     </div>
                 </div>
@@ -110,10 +118,10 @@ const clickback = () => {
 
 
             <!-- Right Side: -->
-            <div class="flex-1 flex flex-col pt-3 p-10">
-                <div class="card-checkin">
+            <div class="flex-1 flex flex-col pt-3 p-10 justify-start items-center ">
+                <div class="card-checkout">
                     <div>
-                        <div class="flex text-2xl font-semibold mt-5 mx-5" style="font-size: 25px;">
+                        <div class="flex text-2xl font-semibold mt-10 mx-5" style="font-size: 25px;">
                             <div class="flex-1 flex flex-col" style="width: 30%;">
                                 <p class="text-center">Check-in</p> <!-- Added text-center class -->
                             </div>
@@ -129,21 +137,52 @@ const clickback = () => {
                     </div>
 
                     <div>
-                        <div class="">
-                            <dl class="grid gap-x-8 gap-y-16 lg:grid-cols-2">
-                                <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-                                    <dt class="text-base leading-7 text-gray-600">Zone</dt>
-                                    <dd class="text-base leading-7 text-gray-600">C</dd>
+                        <div class="flex justify-center items-center mt-10">
+                            <dl class="grid gap-x-0 gap-y-16 lg:grid-cols-3 card-zone"
+                                style="grid-template-columns: 1fr 0fr 1fr;">
+                                <div class="text-left pl-5 px-2">
+                                    <p class="text-sm leading-7 text-gray-600">Zone</p>
+                                    <p class="text-center pb-2">C</p>
                                 </div>
-                                <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-                                    <dt class="text-base leading-7 text-gray-600">Room Number</dt>
-                                    <dd class="text-base leading-7 text-gray-600">701</dd>
+                                <div class="color-line" style="width: 2px;">
+                                    <hr />
+                                </div>
+                                <div class="text-left pl-5 px-2">
+                                    <p class="text-sm leading-7 text-gray-600">Room Number</p>
+                                    <p class="text-center pb-2">701</p>
                                 </div>
                             </dl>
                         </div>
+
+
+                    </div>
+
+
+                </div>
+                <div class="card-checkout mt-5">
+                    <div>
+                        <div class="text-2xl font-semibold mt-10 mx-5" style="font-size: 25px;">
+                            <div class="text-left pl-5 px-2 md-5">
+                                <p class="text-sm leading-7 text-red-600" style="font-size: 17px;">Service *</p>
+                                <!-- Apply text-center class to center the text -->
+                                <p class="text-xs leading-7 text-center text-grey-500 font-light mt-7"
+                                    style="font-size: 16px;">No service</p>
+
+
+                            </div>
+                        </div>
+                        <div class="flex-2 flex flex-row justify-center pt-20" style="margin-top: 20px;">
+                            <button class="btn-checkout">
+                                <a>Check out</a>
+                            </button>
+                        </div>
                     </div>
                 </div>
+
+
             </div>
+
+            \
 
 
 
@@ -176,11 +215,55 @@ const clickback = () => {
     box-shadow: 0px 4px 6px rgb(0 0 0/0.25);
 }
 
-.card-checkin {
-    height: 35vh;
+.card-checkout {
+    width: 80%;
+    max-height: 200px;
+    height: 30vh;
     border-radius: 10px;
-    background-color: rgba(255, 255, 255);
+    background-color: rgb(255, 255, 255);
     box-shadow: 0px 4px 6px rgb(0 0 0/0.25);
 }
-</style>
 
+.card-zone {
+    width: 80%;
+    border-radius: 10px;
+    background-color: rgb(255, 255, 255);
+    box-shadow: 0px 4px 6px rgb(0 0 0/0.25);
+}
+
+
+
+.btn-checkout {
+    background-color: #906843;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 8px;
+    box-shadow: 0px 4px 6px #805d3f;
+    font-weight: medium;
+    text-decoration: none;
+    display: inline-block;
+    width: 60%;
+}
+
+.color-vertical {
+    background-color: #cfcfcf;
+    width: 1px;
+    /* Set width to 1px for vertical lines */
+    height: 90%;
+    /* Set height to 100% for full vertical extent */
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    margin: auto;
+}
+
+.color-line {
+    background-color: #cfcfcf;
+    height: 1px;
+    width: 93%;
+    justify-content: center;
+    display: flex;
+    margin: auto;
+    /* Center the line horizontally */
+}
+</style>
