1f3351354b372057f83ec9a01032726ceb9e3adf
Praewwarin <64160262@go.buu.ac.th.com>
2024-10-26 20:27:53 +0700

    re to ori git

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:3000/',
   headers: {
     'Content-Type': 'application/json',
   },
-
 });
 
 export default http;
