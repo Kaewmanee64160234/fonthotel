6a145f73a4d95d16c1e2c795534522a6e90be453
Praewwarin <64160262@go.buu.ac.th.com>
2024-10-27 14:30:46 +0700

    fix port

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'http://ec2-3-238-191-52.compute-1.amazonaws.com:3000/',
   headers: {
     'Content-Type': 'application/json',
   },
